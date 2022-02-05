79bd0a3265e15240df65200112e46a4b26bae389
luc11zm3 <97666602+luc11zm3@users.noreply.github.com>
2022-02-05 08:01:12 +0700

    update

--- commands/command/User.js
+++ commands/command/User.js
@@ -87,10 +87,8 @@ module.exports = {
 
 	//
 	//console.log(info);
-	if(info.avatar_url === '/icon/mac_dinh.png'){
+	if(info.avatar_url === '/icon/mac_dinh.png' || info.avatar_url.toLowerCase().includes('/avatar/')){
 	    info.avatar_url = 'https://hentaivn.moe'+info.avatar_url
-	}else if(info.avatar_url.toLowerCase().includes('/avatar/')){
-		info.avatar_url = 'https://hentaivn.moe'+info.avatar_url
 	}
 	let uploadText = 'Không';
 	if(info.upload === true){ uploadText = 'Có' }
