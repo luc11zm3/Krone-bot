e1d9d20bb87757819887dd9a3d4348fa1fc8ea40
luc11zm3 <luciashleyokay@gmail.com>
2022-02-14 02:19:21 +0700

    updated

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         px: 'h!',
-        token: 'OTM4OTM5NTc1NjIyMDc0Mzc4.Yfxlnw.RB3V-vGRUM3jIad4_ju6_AA7G6c'
+        token: process.env.TOKEN || 'ODk3ODQyNTczNzQwMjQ1MDMy.YWbjDg.Bss6EB_R0LCKGR29F520JEi7FpU'
         /*px: '!',
         token: 'ODk3ODQyNTczNzQwMjQ1MDMy.YWbjDg.XQigQ8Qi7Hm9Te-G8EgLgtUyYXg'*/
     },
