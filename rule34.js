8f0374df204eb0aac90cdc282afe0f8ae2e8de25
luc11zm3 <luciashleyokay@gmail.com>
2022-03-01 20:13:10 +0700

    updated

--- rule34.js
+++ rule34.js
@@ -14,11 +14,23 @@ const info = {}
 		    const notfound = $(html).find('.content').find('h1').text().trim()
 		    if(notfound.includes('Nobody here but us chickens!')) return 0;
 		    let link = [];
-			const props = $(html).find('.thumb')
-			props.each(function(i,e){
-				link.push('https://rule34.xxx/' + $(this).find('a').attr('href'))
+			let page = ['0',`https://rule34.xxx/index.php?page=post&s=list&tags=${name}+`]
+			const pageProps = $(html).find('.pagination').find('a')
+			pageProps.each(function(){
+				if($(this).attr('href')) page.push('https://rule34.xxx/index.php' + $(this).attr('href'));
 			})
-			let id = Math.floor(Math.random() * props.length+1);
-		return await getImage(link[id]);
+			let Pageid = Math.floor(Math.random() * pageProps.length+1);	
+			async function getImageInPage(page){
+				const response = await axios.get(page)
+				const html = response.data;
+					$ = cheerio.load(html);
+					const props = $(html).find('.thumb')
+					props.each(function(i,e){
+						link.push('https://rule34.xxx/' + $(this).find('a').attr('href'))
+				})
+				let id = Math.floor(Math.random() * props.length+1);
+				return link[id]
+			}
+		return await getImage(await getImageInPage(page[Pageid]))
 }
 module.exports = rule34;
