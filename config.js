1ffb4ebb437d09069066f54fd7adb9f2b0b1f039
luc11zm3 <97666602+luc11zm3@users.noreply.github.com>
2022-04-15 17:23:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 module.exports = {
     app: {
         px: 'h!',
-        token: process.env.TOKEN || 'OTQzNTM3ODY3MjUzMjE5NDI4.Yg0gHQ.ASeT7ZgGfHPzf-VADv7Re790Pcc' ||'ODk3ODQyNTczNzQwMjQ1MDMy.YWbjDg.Bss6EB_R0LCKGR29F520JEi7FpU'
-        /*px: '!',
-        token: 'ODk3ODQyNTczNzQwMjQ1MDMy.YWbjDg.XQigQ8Qi7Hm9Te-G8EgLgtUyYXg'*/
+        token: process.env.TOKEN
     },
     opt: {
         DJ: {
